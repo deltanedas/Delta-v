b16c7771b2ba914fe2cf704655fcf4d531a942a5
deltanedas <@deltanedas:kde.org>
2024-12-19 05:10:22 +0000

    full cleanup of precognition

--- Content.Shared/DeltaV/Abilities/Psionics/PrecognitionPowerComponent.cs
+++ Content.Shared/DeltaV/Abilities/Psionics/PrecognitionPowerComponent.cs
@@ -1,17 +1,14 @@
-using Content.Shared.DoAfter;
 using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
 
 namespace Content.Shared.Abilities.Psionics;
 
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent]
 public sealed partial class PrecognitionPowerComponent : Component
 {
     [DataField]
-    public float RandomResultChance = 0.2F;
-
-    [DataField]
-    public Dictionary<EntityPrototype, PrecognitionResultComponent> AllResults;
+    public float RandomResultChance = 0.2f;
 
     [DataField]
     public SoundSpecifier VisionSound = new SoundPathSpecifier("/Audio/DeltaV/Effects/clang2.ogg");
@@ -19,11 +16,17 @@ public sealed partial class PrecognitionPowerComponent : Component
     [DataField]
     public EntityUid? SoundStream;
 
+    /// <summary>
+    /// The length of the sound effect
+    /// </summary>
     [DataField]
-    public DoAfterId? DoAfter;
+    public TimeSpan UseDelay = TimeSpan.FromSeconds(8.35);
 
+    /// <summary>
+    /// A short delay to give the action if the doafter fails.
+    /// </summary>
     [DataField]
-    public TimeSpan UseDelay = TimeSpan.FromSeconds(8.35); // The length of the sound effect
+    public TimeSpan RetryDelay = TimeSpan.FromSeconds(15);
 
     [DataField]
     public EntProtoId PrecognitionActionId = "ActionPrecognition";
